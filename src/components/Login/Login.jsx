bdd1b36dac0c5ee55c2208d75fed482890e2049a
Jean-baptiste JOFFRE <jeanbaptiste.joffre@gmail.com>
2019-04-04 22:25:04 +0200

    merge

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,51 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Form, Icon, Input, Button, Checkbox, } from 'antd';
+import { createForm } from 'rc-form';
+import { InputItem, List } from 'antd-mobile';
 
 const Login = (props) => {
-    const { getFieldDecorator } = props.form;
+  const { getFieldProps } = props.form;
+  const [ username, setUsername ] = useState("")
+  const [ password, setPassword ] = useState("")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+        window.location.pathname = "/users"
+      }
+    });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-                window.location.pathname = "/users"
-            }
-        });
-    };
-
-    return (
-        <Form onSubmit={handleSubmit} className="login-form">
-            <Form.Item>
-                {getFieldDecorator('username', {
-                    rules: [{ required: true, message: 'Please input your username!' }],
-                })(
-                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-                )}
-            </Form.Item>
-            <Form.Item>
-                {getFieldDecorator('password', {
-                    rules: [{ required: true, message: 'Please input your Password!' }],
-                })(
-                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                )}
-            </Form.Item>
-            <Form.Item>
-                {getFieldDecorator('remember', {
-                    valuePropName: 'checked',
-                    initialValue: true,
-                })(
-                    <Checkbox>Remember me</Checkbox>
-                )}
-                <a className="login-form-forgot" href="">Forgot password</a>
-                <Button type="primary" htmlType="submit" className="login-form-button">
-                    Log in
-                </Button>
-                Or <a href="">register now!</a>
-            </Form.Item>
-        </Form>
-    );
+  return (
+    <List>
+      <InputItem
+        {...getFieldProps('username')}
+        clear
+        placeholder="Username"
+        ref={setUsername}
+      ></InputItem>
+      <InputItem
+        {...getFieldProps('password')}
+        clear
+        placeholder="Password"
+        ref={setPassword}
+      ></InputItem>
+      <List.Item>
+        <div
+          style={{ width: '100%', color: '#108ee9', textAlign: 'center' }}
+          onClick={handleSubmit}
+        >
+          Login
+        </div>
+      </List.Item>
+    </List>
+  );
 };
 
-export default Form.create({ name: 'login' })(Login);
+export default createForm()(Login);
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import User from "./User";
 import history from "../../history"
+import { Button } from "antd-mobile";
 import { ROUTING_ADD, ROUTING_USERS } from '../../constants'
 
 
-function Users({ users }) {
+function Users ({ users }) {
   return (
     <div>
       {users.map(user => <User {...user} key={user.id}/>)}
-      <button onClick={() => history.push(ROUTING_USERS + ROUTING_ADD)
-      }>Ajouter
-      </button>
-
+      <Button type="primary" onClick={() => history.push(ROUTING_USERS + ROUTING_ADD)}>Ajouter</Button>
     </div>
   )
 }
