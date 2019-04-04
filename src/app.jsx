c30efc6e33ae8ce0aa4b8dcc8f74eeee2ac46859
Gauthier POULET <gauthier.poulet@gmail.com>
2019-04-04 19:05:18 +0200

    add style for layout

--- src/app.jsx
+++ src/app.jsx
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux'
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import { Layout, Menu } from 'antd';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faIgloo,faMedkit } from '@fortawesome/free-solid-svg-icons'
+import { faIgloo, faMedkit } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faIgloo)
 library.add(faMedkit)
-
 
 
 import { createStore, applyMiddleware, compose } from 'redux';
@@ -31,51 +30,54 @@ const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
 ));
 
 
-
 const {
   Header, Content,
 } = Layout;
 
 
 const Index = () => {
-    return (
-        <h1><FontAwesomeIcon icon="medkit" /> Index</h1>
-    );
+  return (
+    <h1><FontAwesomeIcon icon="medkit"/> Index</h1>
+  );
 };
 
 const App = () => {
-    return (
-      <Provider store={store}>
-          <Router>
-                <Layout>
-                    <Header>
-                        <Menu
-                            theme="dark"
-                            mode="horizontal"
-                            defaultSelectedKeys={['2']}
-                            style={{ lineHeight: '64px' }}
-                        >
-                            <Menu.Item key="1">nav 1</Menu.Item>
-                            <Menu.Item key="2">nav 2</Menu.Item>
-                            <Menu.Item key="3">nav 3</Menu.Item>
-                        </Menu>
-                    </Header>
-                    <Layout>
-                        <Content>
-                            <Route path="/" exact component={Index} />
-                            <Route path={ROUTING_USERS}  component={Users} />
-                            <Route path={ROUTING_USERS+"/:userId"}  component={Parkours} />
-                        </Content>
-                    </Layout>
-                </Layout>
-          </Router>
-      </Provider>
-    )
+  return (
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <Header>
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item key="1">nav 1</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+          </Header>
+          <Layout>
+            <Content>
+              <div style={{ background: '#ECECEC', padding: '30px' }}>
+                <Switch>
+                  <Route path="/" exact component={Index}/>
+                  <Route path={ROUTING_USERS + "/:userId"} exact component={Parkours}/>
+                  <Route path={ROUTING_USERS} component={Users}/>
+                </Switch>
+              </div>
+            </Content>
+          </Layout>
+        </Layout>
+      </Router>
+    </Provider>
+  )
 };
 
 export const mount = (id) => {
-    ReactDOM.render(
-        <App/>,
-        document.getElementById(id)
-    )
+  ReactDOM.render(
+    <App/>,
+    document.getElementById(id)
+  )
 };
