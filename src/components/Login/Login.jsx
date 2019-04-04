35950e80fa8f3d01b9f5d304b73c94bba401c21a
Jean-baptiste JOFFRE <jeanbaptiste.joffre@gmail.com>
2019-04-04 21:20:34 +0200

    add login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { Form, Icon, Input, Button, Checkbox, } from 'antd';
+
+const Login = (props) => {
+    const { getFieldDecorator } = props.form;
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+                window.location.pathname = "/users"
+            }
+        });
+    };
+
+    return (
+        <Form onSubmit={handleSubmit} className="login-form">
+            <Form.Item>
+                {getFieldDecorator('username', {
+                    rules: [{ required: true, message: 'Please input your username!' }],
+                })(
+                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                )}
+            </Form.Item>
+            <Form.Item>
+                {getFieldDecorator('password', {
+                    rules: [{ required: true, message: 'Please input your Password!' }],
+                })(
+                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                )}
+            </Form.Item>
+            <Form.Item>
+                {getFieldDecorator('remember', {
+                    valuePropName: 'checked',
+                    initialValue: true,
+                })(
+                    <Checkbox>Remember me</Checkbox>
+                )}
+                <a className="login-form-forgot" href="">Forgot password</a>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                    Log in
+                </Button>
+                Or <a href="">register now!</a>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export default Form.create({ name: 'login' })(Login);
