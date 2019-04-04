291b21a48939002957a974b72caa02448589f722
Jean-baptiste JOFFRE <jeanbaptiste.joffre@gmail.com>
2019-04-04 16:21:53 +0200

    add router

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Layout } from 'antd';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import { Layout, Menu } from 'antd';
 
 const {
-    Header, Content, Footer, Sider,
+    Header, Content,
 } = Layout;
 
+const Index = () => {
+    return (
+        <h1>Index</h1>
+    );
+};
+
 const App = () => {
-    return (<Layout>
-        <Header>header</Header>
-        <Layout>
-          <Sider>left sidebar</Sider>
-          <Content>main content</Content>
-          <Sider>right sidebar</Sider>
-        </Layout>
-        <Footer>footer</Footer>
-      </Layout>)
+    return (
+        <Router>
+            <Layout>
+                <Header>
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        style={{ lineHeight: '64px' }}
+                    >
+                        <Menu.Item key="1">nav 1</Menu.Item>
+                        <Menu.Item key="2">nav 2</Menu.Item>
+                        <Menu.Item key="3">nav 3</Menu.Item>
+                    </Menu>
+                </Header>
+                <Layout>
+                    <Content>
+                        <Route path="/" exact component={Index} />
+                    </Content>
+                </Layout>
+            </Layout>
+        </Router>
+    )
 };
 
 export const mount = (id) => {
     ReactDOM.render(
-        <App />,
+        <App/>,
         document.getElementById(id)
     )
 };
