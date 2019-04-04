349a3938ceccf4ae6b3e3329bc29d53a2999835b
Jean-baptiste JOFFRE <jeanbaptiste.joffre@gmail.com>
2019-04-04 23:38:56 +0200

    add drawer

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux'
 
-import { Router, Route, NavLink, Switch, Redirect } from "react-router-dom";
+import { Router, Route, Link, Switch, Redirect } from "react-router-dom";
 
-import { WingBlank, NavBar, Icon, WhiteSpace, Popover } from 'antd-mobile';
-
-const Item = Popover.Item;
+import { WingBlank, NavBar, WhiteSpace, Icon, Drawer, List } from 'antd-mobile';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faIgloo, faMedkit } from '@fortawesome/free-solid-svg-icons'
@@ -38,8 +36,38 @@ const store = createStore(rootReducer(history), /* preloadedState, */ composeEnh
   applyMiddleware(thunk, routerMiddleware(history))
 ));
 
+
+const data = [
+  {
+    value: '2',
+    label: 'Famille',
+    link: '/users'
+  },
+  {
+    value: '3',
+    label: 'Agenda',
+    link: '/agenda'
+  },
+  {
+    value: '4',
+    label: 'Documents',
+    link: '/documents'
+  },
+];
+
+const Sidebar = () => {
+    return data.map(
+      (item, index) => {
+        return <List.Item key={index}>
+          <Link to={item.link} >{item.label}</Link>
+        </List.Item>
+      }
+    )
+};
+
 const App = () => {
   const [ splash, setSplash ] = useState(true);
+  const [ isMenuVisible, setIsMenuVisible ] = useState(false);
 
   useEffect(() => {
     if (splash) {
@@ -48,27 +76,43 @@ const App = () => {
         clearInterval(timer);
       }, 1000);
     }
-  }, [splash]);
+  }, [ splash ]);
 
   if (splash) return <Splash/>
 
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-      <Router history={history}>
-        <NavBar mode="light"></NavBar>
-        <WhiteSpace size="lg" />
-        <WingBlank>
-          <Switch>
-            <Route path="/login" exact component={Login}/>
-            <Route path={ROUTING_USERS + ROUTING_ADD} exact component={UserAdd}/>
-            <Route path={ROUTING_USERS + "/:userId"} exact component={Parkours}/>
-            <Route path={ROUTING_USERS} component={Users}/>
-            <Route path={ROUTING_AGENDA} exact component={Agenda}/>
-            <Redirect from="/" to="/login"/>
-          </Switch>
-        </WingBlank>
-      </Router>
+        <Router history={history}>
+          <NavBar className="top-nav-bar"
+                  icon={<Icon type="ellipsis" />}
+                  onLeftClick={(...args) => {
+                    console.log(args);
+                    setIsMenuVisible(!isMenuVisible);
+                  }}></NavBar>
+          <Drawer
+            className="my-drawer"
+            style={{ minHeight: document.documentElement.clientHeight }}
+            //enableDragHandle
+            contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
+            sidebar={<Sidebar />}
+            docked={isMenuVisible}
+            open={isMenuVisible}
+          >
+
+          </Drawer>
+          < WhiteSpace size="lg"/>
+          <WingBlank>
+            <Switch>
+              <Route path="/login" exact component={Login}/>
+              <Route path={ROUTING_USERS + ROUTING_ADD} exact component={UserAdd}/>
+              <Route path={ROUTING_USERS + "/:userId"} exact component={Parkours}/>
+              <Route path={ROUTING_USERS} component={Users}/>
+              <Route path={ROUTING_AGENDA} exact component={Agenda}/>
+              <Redirect from="/" to="/login"/>
+            </Switch>
+          </WingBlank>
+        </Router>
       </ConnectedRouter>
     </Provider>
   )
