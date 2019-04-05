a5a2abf6b9338e4a09876f4706b2c3383e8e25fc
Gauthier POULET <gauthier.poulet@gmail.com>
2019-04-05 03:18:34 +0200

    add document in an event, la fin

--- src/components/Timeline/Document.jsx
+++ src/components/Timeline/Document.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Icon } from 'antd'
 
-function Document({ id }) {
+function Document({ name, file }) {
   return (
     <div>
-      {id}
+      <a href={file} target="_blank" rel="noreferrer noopener">
+      <Icon type="file-pdf" />&nbsp;{name}
+      </a>
     </div>
   )
 }
