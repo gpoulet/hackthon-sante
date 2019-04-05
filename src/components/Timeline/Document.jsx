563c83a8c552f4ef7dd90a85ed280a9a58508491
Gauthier POULET <gauthier.poulet@gmail.com>
2019-04-05 04:09:38 +0200

    fix warnings

--- src/components/Timeline/Document.jsx
+++ src/components/Timeline/Document.jsx
@@ -3,11 +3,11 @@ import { Icon } from 'antd'
 
 function Document({ name, file }) {
   return (
-    <div>
+    <p>
       <a href={file} target="_blank" rel="noreferrer noopener">
       <Icon type="file" />&nbsp;{name}
       </a>
-    </div>
+    </p>
   )
 }
 
