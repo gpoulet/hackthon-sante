c1df91c6169ca1e24703c1ec462f71464d4ecca8
Gauthier POULET <gauthier.poulet@gmail.com>
2019-04-05 07:02:23 +0200

    fix title

--- src/components/Timeline/Document.jsx
+++ src/components/Timeline/Document.jsx
@@ -3,11 +3,11 @@ import { Icon } from 'antd'
 
 function Document({ name, file }) {
   return (
-    <p>
+    <>
       <a href={file} target="_blank" rel="noreferrer noopener">
       <Icon type="file" />&nbsp;{name}
       </a>
-    </p>
+    </>
   )
 }
 
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -9,6 +9,7 @@ import { ROUTING_ADD, ROUTING_USERS } from '../../constants'
 function Users ({ users }) {
   return (
     <div>
+      <h2><Icon type="home"/> Membres de la famille</h2>
       {users.map(user => <User {...user} key={user.id}/>)}
       <Button type="primary" onClick={() => history.push(ROUTING_USERS + ROUTING_ADD)}><Icon type="user-add" />&nbsp;&nbsp;Ajouter</Button>
     </div>
