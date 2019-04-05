d50130f7afe2b4266f1eef72d1d359b8c14bbbd5
Gauthier POULET <gauthier.poulet@gmail.com>
2019-04-05 03:11:17 +0200

    add document in an event, la suite

---
+++ src/components/Timeline/Document.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Document({ id }) {
+  return (
+    <div>
+      {id}
+    </div>
+  )
+}
+
+export default Document
--- src/components/Timeline/TimelineItem.jsx
+++ src/components/Timeline/TimelineItem.jsx
@@ -3,16 +3,21 @@ import { Timeline } from 'antd';
 
 import { formatDDMMYYYY } from '../../util'
 import { getColorFromType } from '../../constants'
+import Document from './Document.js'
 
-function TimelineItem({ parkoursId, type, date, title }) {
+function TimelineItem({ parkoursId, type, date, title, documents }) {
   return (
     <Timeline.Item
       color={getColorFromType(type, date)}>{formatDDMMYYYY(date)} : {title}
       <p>
-
+        {documents.map((document, index) => <Document key={index} id={document.id}/>)}
       </p>
     </Timeline.Item>
   )
+}
+
+TimelineItem.defaultProps = {
+  documents: []
 }
 
 export default TimelineItem
