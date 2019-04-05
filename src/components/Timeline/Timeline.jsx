566b2bee110b933cb17bcb197679b747a20adedd
Gauthier POULET <gauthier.poulet@gmail.com>
2019-04-05 02:00:42 +0200

    improve timeline

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { object } from "prop-types";
 import { Timeline } from 'antd';
+import { Card, WingBlank, WhiteSpace } from 'antd-mobile';
 import { formatDDMMYYYY } from '../../util'
 import { getColorFromType } from '../../constants'
 
 function TimelinePage({ timeline }) {
   const { title, events } = timeline;
   return (
-    <div>
-      <p>{ title }</p>
-    <Timeline>
-      { events.map(event => <Timeline.Item color={getColorFromType(event.type, event.date)}>{formatDDMMYYYY(event.date)} : {event.title}</Timeline.Item>) }
-    </Timeline>
-    </div>);
+    <WingBlank size="lg">
+      <WhiteSpace size="lg"/>
+      <Card>
+        <Card.Header
+          title={title}
+        />
+        <Card.Body>
+          <Timeline>
+            {events.map(event => <Timeline.Item
+              color={getColorFromType(event.type, event.date)}>{formatDDMMYYYY(event.date)} : {event.title}</Timeline.Item>)}
+          </Timeline>
+        </Card.Body>
+      </Card>
+    </WingBlank>);
 }
 
 TimelinePage.propTypes = {
